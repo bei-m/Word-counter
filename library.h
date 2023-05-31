e012548d1511ae35e79000457ae7171328267398
bei-m <116886672+bei-m@users.noreply.github.com>
2023-05-31 12:25:28 +0300

    Add files via upload

---
+++ library.h
@@ -0,0 +1,37 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <map>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <ctype.h>
+#include <regex>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::ifstream;
+using std::string;
+using std::map;
+using std::cin;
+using std::set;
+using std::stringstream;
+using std::transform;
+using std::ofstream;
+using std::setw;
+using std::back_inserter;
+using std::fill_n;
+using std::to_string;
+using std::left;
+using std::find;
+using std::regex;
+
+
+
+#endif
